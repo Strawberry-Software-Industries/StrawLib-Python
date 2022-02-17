f52b461f0b4deb4ab177254235030e74368051cb
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-02-17 18:59:04 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 import psutil
 import time
+from colorama import Fore
 
 def libinfo():
-    print("v1")
+    print(f"{Fore.RED}StrawLib{Fore.RESET} {Fore.GREEN}1.0{Fore.RESET}")
+    print(f"{Fore.RED}Build{Fore.RESET} 2022-02-17")
+    print(f"{Fore.CYAN}Copyright (C) 2022 Strawberry Software Industries{Fore.RESET}")
---
+++ utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+class config:
+
+    def read(cfg_file=""):
+        with open(cfg_file, "r") as f:
+            data = f.read()
+
+        print(data.rstrip())
+
+    def write(cfg_file="", context=""):
+        f = open(cfg_file, "w")
+        f.write(context)
+        f.close
+
+
+class file:
+    
+    def delete(file=""):
+        os.remove(file)
+        
+
+    def create(file=""):
+        open(file, "x")
